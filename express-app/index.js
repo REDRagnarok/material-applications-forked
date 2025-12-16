05d819b34ba80d07a5e29048efe35a84001769f5
REDRagnarok <60975957+REDRagnarok@users.noreply.github.com>
2025-12-16 17:57:27 +0300

    Update index.js

--- express-app/index.js
+++ express-app/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Happy New Year World!</h1>')
+  res.send('<h1>Happy World!</h1>')
 })
 
 const PORT = 8080
